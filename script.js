0d7d7dd7d4c07717a4fd44d4be16b2f5cc6b511b
EliteGbohunmi <elitegbohunmi@gmail.com>
2025-01-10 23:54:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+ScrollReveal().reveal('.rollo', {
+    rotate: {
+        x: 20,
+        z: 20
+    },
+    delay: '700' 
+});
+ScrollReveal().reveal('.anima', {
+    distance: '50px',
+    origin: 'left',
+    delay: '600'
+})
+ScrollReveal().reveal('.hmm', {
+    distance: '50px',
+    origin: 'right',
+    delay: '600'
+})
+ScrollReveal().reveal('.anima', {
+    distance: '50px',
+    origin: 'left',
+    delay: '600'
+})
+ScrollReveal().reveal('.nav', {
+    distance: '50px',
+    origin : 'top'
+})
+ScrollReveal().reveal('.one', {
+    distance: '50px',
+    origin: 'top',
+    delay : '600'
+})
+ScrollReveal().reveal('.two', {
+    distance: '50px',
+    origin: 'top',
+    delay : '700'
+})
+ScrollReveal().reveal('.three', {
+    distance: '50px',
+    origin: 'top',
+    delay : '800'
+})
